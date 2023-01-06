d29b44896976f35e24a3b0516775d28119ed5269
ShivangM <shivangmishra0824@gmail.com>
2023-01-06 22:42:58 +0530

    Tailwind INIT

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div className="">Home</div>;
+  return <div className="text-rose-500">Home</div>;
 };
 
 export default Home;
