cc45c608588bb99efe64eac361bda28aede47406
ShivangM <shivangmishra0824@gmail.com>
2023-01-08 03:18:38 +0530

    Support us fix

--- pages/support-us.tsx
+++ pages/support-us.tsx
@@ -18,8 +18,11 @@ const SupportUs = (props: Props) => {
 
   const { data, sendTransaction } = useSendTransaction({
     ...config,
-    onSuccess: (data) => console.log('SUCCESS', data),
-    onError: (error) => toast.error(error),
+    onSuccess: (data) =>
+      toast.success(
+        `Donated ${value} MATIC sucessfully, Thank you for your donation.`
+      ),
+    onError: (error) => toast.error('Oops, some error occured'),
     onSettled: (data, error) => console.log('SETTLED', data, error),
   });
 
@@ -32,53 +35,40 @@ const SupportUs = (props: Props) => {
               className="h-1/2 w-full object-cover pl-4 pt-4 pb-4 pr-4 rounded-lg"
               src="https://media.istockphoto.com/id/1220167694/vector/five-women-of-different-nationalities-and-cultures-standing-together.jpg?s=612x612&w=0&k=20&c=74IWmqUDN3BfdaKZwBf6aub-qiXR25m_jp0UALuWdgQ="
             />
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-cascade-800">
+            <h2 className="mb-4 text-4xl tracking-tight font-medium text-center text-cascade-800">
               Support Us
             </h2>
-            <p className="px-4 text-cascade-800 font-extrabold">Our Mission</p>
+            <p className="px-4 text-cascade-800 font-medium">Our Mission</p>
             <p className="mt-4 px-4 text-gray-500">
-              This charitable trust works for women's empowerment. We are
-              committed to the growth of women in corporate sectors and making
-              them confident and independent. The aim remains to have efficient
-              opportunities for them so that they can achieve their optimum
-              potential.{' '}
+              SheConfident is a platform that empowers women by providing
+              resources to boost self-esteem and confidence, as well as listing
+              their projects for crowdfunding. Our goal is to help women achieve
+              their dreams by offering a range of tools and support services.
             </p>
           </div>
 
           <div className="h-full p-3 rounded-lg w-full bg-white">
             <p className="font-semibold text-xl mt-2 text-cascade-800 text-center">
-              Where would you like to give?
+              Here are ways you can support us through:
             </p>
             <div className="mt-4 w-full flex flex-col">
               <div className="flex gap-2 w-full">
-                <input
-                  className="appearance-none"
-                  type="radio"
-                  name="select_option"
-                  id="select_option1"
-                />
                 <label className="w-full">
                   <div className="click_option h-16 gap-3 cursor-pointer transition-all w-full px-2 border-2 flex justify-center items-center">
                     <img className="w-8" src="https://imgur.com/8n0nocQ.png" />
                     <p className="text-sm text-cascade-800 font-semibold">
-                      Where it's needed most
+                      Donating some ETH to us.
                     </p>
                   </div>
                 </label>
               </div>
 
               <div className="flex gap-2 w-full mt-4">
-                <input
-                  className="appearance-none"
-                  type="radio"
-                  name="select_option"
-                  id="select_option3"
-                />
                 <label className="w-full">
                   <div className="click_option h-16 gap-3 cursor-pointer transition-all w-full px-2 border-2 flex justify-center items-center">
                     <img className="w-8" src="https://imgur.com/BHkXQhS.png" />
                     <p className="text-sm text-cascade-800 font-semibold">
-                      Research
+                      Help us out by reporting bugs
                     </p>
                   </div>
                 </label>
@@ -93,7 +83,7 @@ const SupportUs = (props: Props) => {
             <p className="mt-3 font-semibold text-cascade-800">
               I would like to give:
             </p>
-            <div className="flex -mt-5 gap-2">
+            <div className="flex mt-3 gap-2">
               <label
                 onClick={() => setValue('1')}
                 className="h-10 text-gray-700 font-semibold text-sm cursor-pointer transition-all justify-center items-center w-full border-2 flex "
@@ -126,7 +116,8 @@ const SupportUs = (props: Props) => {
                 type="number"
                 min={0.1}
                 max={1}
-                className="w-full border"
+                className="w-full py-2 border border-black rounded mt-3 px-3"
+                placeholder="Please type the amount you want to donate (0.1 - 1 MATIC)"
                 onChange={(e) => {
                   setValue(e.target.value);
                 }}
@@ -136,14 +127,14 @@ const SupportUs = (props: Props) => {
               {value} MATIC
             </p>
             <hr className="my-4" />
-            <div className=" flex gap-3 items-center">
+            {/* <div className=" flex gap-3 items-center">
               <span className="check_it h-6 w-6 border border-gray-500 rounded flex justify-center items-center cursor-pointer transition-all ">
                 <i className=" text-white fa fa-check"></i>
               </span>
               <p className="text-sm font-semibold  text-gray-700">
                 This gift is in honour or in memory
               </p>
-            </div>
+            </div> */}
             <div className="my-4 flex justify-between">
               <button
                 onClick={() => sendTransaction?.()}
