b46721d8f6fbb94bd7b5d0ca5180848a050537e6
ShivangM <shivangmishra0824@gmail.com>
2023-08-08 00:05:19 +0530

    Fix for window not found error

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -17,6 +16,5 @@ const firebaseConfig = {
 const apps = getApps();
 const app = !apps.length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
-export const analytics = getAnalytics(app);
 
 export default db;
